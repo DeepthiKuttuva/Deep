a9ec7f4fe69e90c6026ce279cee7e5a8c584eef9
Deepthi <deepthi.aries@gmail.com>
2022-09-16 15:06:41 -0700

    guru99 test

---
+++ automation/GitTest2/src/test/java/Deep2/GitTest2/guru99Test.java
@@ -0,0 +1,32 @@
+package Deep2.GitTest2;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class guru99Test {
+	
+	public WebDriver driver;
+	@Test
+	public void guru99tutorials() throws InterruptedException{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\venka\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver=new ChromeDriver();
+		String eTitle="Meet Guru99";
+		String aTitle=driver.getTitle();	
+		driver.get("http://www.guru99.com");
+		driver.manage().window().maximize();
+		if(eTitle.equals(aTitle)) {
+			System.out.println("Test pass");
+		}
+		else {
+			System.out.println("Test fail");
+		}
+		driver.close();
+	}}
+
+
+
+
+
+
+
