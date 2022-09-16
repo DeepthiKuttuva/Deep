abfdbbea229447a44f554c890a0b3daa2ef7be93
Deepthi <deepthi.aries@gmail.com>
2022-09-16 14:04:43 -0700

    guru99

---
+++ automation/gittest1/src/main/java/deep1/gittest1/App.java
@@ -0,0 +1,13 @@
+package deep1.gittest1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
