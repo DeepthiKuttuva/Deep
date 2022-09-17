9f64bcfa8569664ece11d6db66e95d425ccdf2bf
DeepthiKuttuva <deepthi.aries@gmail.com>
2022-09-16 17:12:58 -0700

    checking new code

---
+++ automation/BddRestAssured/src/test/java/com/qa/rest/tests/GetCall.java
@@ -0,0 +1,28 @@
+package com.qa.rest.tests;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class GetCall {
+     
+	@Test
+	public void test_NoOfCircuitsFor2017Season() {
+		
+		
+		given().
+		when().
+		    get("http://ergast.com/api/f1/2017/circuits.json").
+		then().
+		assertThat().
+		body("MRData.CircuitTable.Circuits.circuitId",hasSize(20)).
+		and().
+		statusCode(200).
+		and().
+		header("Content-Length",equalTo("4551"));
+
+	    
+		
+	}
+	
+}
